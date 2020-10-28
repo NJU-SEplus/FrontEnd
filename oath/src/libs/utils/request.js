5b14d25f9387f57cfd940705885aa5d2ba39ab23
wxj <kingsnowmushroom@126.com>
2020-10-28 16:16:06 +0800

    add: request env

--- oath/src/libs/utils/request.js
+++ oath/src/libs/utils/request.js
@@ -1,31 +1,53 @@
-import axios from 'axios'
-import qs from 'qs'
-let fetch = axios.create({
-    baseURL: "http://120.55.43.25:8088/", 
-    timeout: 50000 // request timeout
-})
-fetch.interceptors.request.use(config => {
-    if (config.method === 'post' || config.method === 'put' || config.method === 'delete') {
-        if (typeof(config.data) !== 'string' && config.headers['Content-Type'] !== 'multipart/form-data') {
-            config.data = qs.stringify(config.data)
-        }
-    }
-    return config
-}, error => {
-    Promise.reject(error)
-})
+import axios from "axios";
+import qs from "qs";
 
-fetch.interceptors.response.use(async data => {
-    return data
-}, error => {
-    if (error.response) {
-        if (error.response.status === 500) {
-            console.log('服务器错误，请联系管理员处理')
-        }
-        return Promise.reject(error.response.data)
-    } else {
-        return Promise.reject(error)
+let env = process.env.NODE_ENV;
+
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : process.env.NODE_ENV === "production"
+    ? "http://120.55.43.25:8088/"
+    : "";
+let fetch = axios.create({
+  baseURL: baseURL,
+  timeout: 50000, // request timeout
+});
+fetch.interceptors.request.use(
+  (config) => {
+    if (
+      config.method === "post" ||
+      config.method === "put" ||
+      config.method === "delete"
+    ) {
+      if (
+        typeof config.data !== "string" &&
+        config.headers["Content-Type"] !== "multipart/form-data"
+      ) {
+        config.data = qs.stringify(config.data);
+      }
     }
-})
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+fetch.interceptors.response.use(
+  async (data) => {
+    return data;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 500) {
+        console.log("服务器错误，请联系管理员处理");
+      }
+      return Promise.reject(error.response.data);
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
 
 export default fetch;
