8cf48493448f9fc929c5318535d30133390ab4b6
wxj <kingsnowmushroom@126.com>
2020-12-12 23:24:17 +0800

    add：审稿人推荐

---
+++ oath/src/pages/ReviewerRec.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ReviewerRec extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div>Review Rec</div>;
+  }
+}
+
+export default ReviewerRec;
